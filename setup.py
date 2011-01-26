5d203f39c524ce3be2351ea43ccfd277d164c1c2
Ralph Bean <ralph.bean@gmail.com>
2011-01-26 10:16:09 -0500

    imported old project by John

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ version = '0.1'
 setup(name='hardlinker',
       version=version,
       description="Find identical files and hardlink them",
-      long_description="""\
-""",
+      long_description="""Forked from hardlink.py by John L. Villalovos"""
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='John Villalovos, Ralph Bean',
+      author='Ralph Bean',
       author_email='ralph.bean@gmail.com',
       url='http://github.com/ralphbean/hardlinker',
       license='GPLv3+',
